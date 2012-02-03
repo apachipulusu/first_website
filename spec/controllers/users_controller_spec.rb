0fb273c3764ddd6e86bf045a96d64ac0b6fd6911
apachipulusu <apachipulusu@tribune.com>
2012-02-03 10:52:04 -0600

    finished layout and routes

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe UsersController do
+  render_views
+
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+       get 'new'
+       response.should have_selector("title", :content => "Sign up")
+    end
+    
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get 'new'
+    response.should have_selector('title', :content => "Sign up")
+  end
+end
